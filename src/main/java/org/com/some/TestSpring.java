7c3074156b84f5250ee2c0505fcac0da3ff22fc7
some <xxvikingxx@yandex.ru>
2024-09-20 20:10:54 +0300

    Hello world!

---
+++ src/main/java/org/com/some/TestSpring.java
@@ -0,0 +1,15 @@
+package org.com.some;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+        context.start();
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
+    }
+}
